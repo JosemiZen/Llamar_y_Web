e3fc9b9b3f46f1544b166f86f513ea2fbdb7d917
Josemi Gonzalez <xemy.013@gmail.com>
2017-11-20 13:32:10 +0100

    fin de trabajo 20/11

--- app/src/main/java/com/example/xemy0/app_webllamar/MainActivity.java
+++ app/src/main/java/com/example/xemy0/app_webllamar/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.xemy0.app_webllamar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void accederWeb (View View) {
+
+        String url =
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
